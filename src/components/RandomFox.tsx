1dcc5207ece372799108741ea3e175af55226637
Pablo Vallejos <faktory.fm@live.com>
2024-02-15 17:08:57 -0300

    feat: state con tipos personalizados

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,8 +1,6 @@
-//{"image":"https:\/\/randomfox.ca\/images\/25.jpg","link":"https:\/\/randomfox.ca\/?i=25"}
-//npm run dev
-const random =()=>Math.floor(Math.random()*123)+1;
 
-export const RandomFox=():JSX.Element=>{
-    const image:string = `https://randomfox.ca/images/${random()}.jpg`
-    return <img width={320} height="auto" className="rounded"  src={image} alt="fox"/>
+type Props={image:string};
+export const RandomFox=(props:Props):JSX.Element=>{
+    
+    return <img width={320} height="auto" className="rounded"  src={props.image} alt="fox"/>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { RandomFox } from "@/components/RandomFox";
+//genera un id unico
+const generateId=()=>Math.random().toString(36).substr(2,9);
+const random = () => Math.floor(Math.random() * 123) + 1;
+//type ImageItema=Array<{id:string;url:string}>
+type ImageItems={id:string;url:string}
+
 const Home: NextPage = () => {
+  const [images, setImages] = useState<Array<ImageItems>>([
+    {id:generateId(),url:`https://randomfox.ca/images/${random()}.jpg`},
+    {id:generateId(),url:`https://randomfox.ca/images/${random()}.jpg`},
+    {id:generateId(),url:`https://randomfox.ca/images/${random()}.jpg`},
+    {id:generateId(),url:`https://randomfox.ca/images/${random()}.jpg`}
+  ]);
   return (
     <div >
       <Head>
@@ -12,7 +25,10 @@ const Home: NextPage = () => {
       </Head>
       <main >
         <h1 className="text-3xl font-bold underline">hola mundo</h1>
-        <RandomFox/>
+        {images.map((image) => (
+          <div key={image.url} className="p-4"> <RandomFox image={image.url} /></div>
+        ))}
+
       </main>
       <footer></footer>
     </div>
