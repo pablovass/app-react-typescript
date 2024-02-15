bb3a0c88857dd30ba3202efb00fb0a5990d2706d
Pablo Vallejos <faktory.fm@live.com>
2024-02-15 10:58:19 -0300

    feat: implementado el componente RandomFox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { RandomFox } from "@/components/RandomFox";
 const Home: NextPage = () => {
   return (
     <div >
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
       </Head>
       <main >
         <h1 className="text-3xl font-bold underline">hola mundo</h1>
+        <RandomFox/>
       </main>
       <footer></footer>
     </div>
